29a4f49fd393ebcbb8a449c63b654fe2886fcd64
Lívia Žitňanská <49245534+liviazitnanska@users.noreply.github.com>
2022-11-09 14:06:22 +0100

    Add files via upload

---
+++ src/main/java/com/pdb_db/pdb_proj/PdbProjApplication.java
@@ -0,0 +1,13 @@
+package com.pdb_db.pdb_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdbProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdbProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdb_db/pdb_proj/PdbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdb_db.pdb_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdbProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
