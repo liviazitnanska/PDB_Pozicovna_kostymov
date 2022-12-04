5d6c5daf40a38d58518d9bf085753688cabb2624
xbalif00 <xbalif00@stud.fit.vutbr.cz>
2022-12-04 11:31:02 +0100

    Update mapping in Mongo

--- src/main/java/com/pdb_db/pdb_proj/domainMongo/recenzia_doplnokMongo/RecenziaDoplnokControllerM.java
+++ src/main/java/com/pdb_db/pdb_proj/domainMongo/recenzia_doplnokMongo/RecenziaDoplnokControllerM.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/recenziadoplnokM")
+@RequestMapping("api/v1/recenzia_doplnokM")
 @AllArgsConstructor
 public class RecenziaDoplnokControllerM
 {
